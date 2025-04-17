4cf57d8e7c0a1be67b47319fbef6e225fd44071c
KinkZilla <realcjbaker@gmail.com>
2025-04-17 14:03:07 -0700

    Update App Start.js (Remove deprecated code

--- App Start.js
+++ App Start.js
@@ -1,10 +1,4 @@
 // Calls the "onStart" function on app start.
 onStart();
 
-//Testing tip bar
-$overlay.emit("goalUpdate", {
-  current: $kv.get('goalCurrent'),
-  value: $kv.get('goalValue')
-});
-
 
