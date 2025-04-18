396a2b4ee711e847117a05bf10f7f4e44586d664
KinkZilla <realcjbaker@gmail.com>
2025-04-18 10:03:16 -0700

    Update Callback.js
    Add fallback logic for DelayedSubjectFix callback; ensure updateSubject runs safely after app start. Add third callback for 'winner' notice

--- Callback.js
+++ Callback.js
@@ -1,14 +1,16 @@
-
-
 if ($callback.label === 'SessionLeaderAnnounce') {
   broadcastSessionLeaderboard();
-}
+} else {
+  if ($callback.label === 'Intro') {
+    tipToStartCallback();
+  } else if ($callback.label === 'Main') {
+    $callback.cancel('Intro');
+    sendDaddyNoticeTo();
+  
+  } else if ($callback.label === 'PrizeNotice') {
+    $callback.cancel('Main');
+    $callback.cancel('Intro');
 
-if ($callback.label === 'Main'){
-  $callback.cancel('Intro')
-  sendDaddyNoticeTo();
-}
-
-if ($callback.label === 'Intro') {
-  tipToStartCallback();
+    firePrizeNotice();
+  }
 }
