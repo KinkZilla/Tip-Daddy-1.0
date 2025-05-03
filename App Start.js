19d62d37f604a30e9debacc81bf82d2867bb7b90
KinkZilla <realcjbaker@gmail.com>
2025-05-03 12:20:29 -0700

    Update App Start.js NEW version 1.0.5

--- App Start.js
+++ App Start.js
@@ -1,4 +1,11 @@
-// Runs on app start.
-onStart();
-
+if ($kv.get('appSessionInitialized') === false) {
+  initializeAppSessionState(); 
+}
+     // 🧱 Prep all KV
+sendMsg('appStarted');
+// ✅ After all that, THEN create the introDaddyPrompt callback
+if ($kv.get('crownUnlocked') !== 'true') {
+  $callback.cancel('introDaddyPrompt');
+  $callback.create('introDaddyPrompt', 60 * TIME_VAR, true); // 🔁 Loop reminder
+}
 
