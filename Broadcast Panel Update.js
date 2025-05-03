9880078059dd2d0ac864cb542c574c32464fdee4
KinkZilla <realcjbaker@gmail.com>
2025-05-03 12:21:27 -0700

    Update Broadcast Panel Update.js NEW version 1.0.5

--- Broadcast Panel Update.js
+++ Broadcast Panel Update.js
@@ -1,40 +1,13 @@
-let currentAllTime = 'None - 0 tokens';
-let goalCurrent = 0;
-let goalValue = 0;
-let currentDaddy = 'None';
+const row1 = $kv.get('panel_row1_value') || '';
+const row2 = $kv.get('panel_row2_value') || '';
+const row3 = $kv.get('panel_row3_value') || '';
 
-try {
-  currentAllTime = $kv.get('currentAllTime') || 'None - 0 tokens';
-} catch (e) {
-  currentAllTime = 'None - 0 tokens';
-}
-
-try {
-  goalCurrent = Number($kv.get('goalCurrent') || 0);
-} catch (e) {
-  goalCurrent = 0;
-}
-
-try {
-  goalValue = Number($kv.get('goalValue') || 0);
-} catch (e) {
-  goalValue = 0;
-}
-
-try {
-  currentDaddy = $kv.get('currentDaddyIs') || 'None';
-} catch (e) {
-  currentDaddy = 'None';
-}
-
-const options = {
+$room.setPanelTemplate({
   template: '3_rows_of_labels',
-  row1_label: 'Goal',
-  row1_value: goalCurrent + ' of ' + goalValue,
-  row2_label: "Current Daddy",
-  row2_value: currentDaddy,
-  row3_label: "All Time Daddy 🏆",
-  row3_value: currentAllTime
-};
-
-$room.setPanelTemplate(options);
+  row1_label: 'Received / Goal',
+  row1_value: row1,
+  row2_label: 'Current Daddy',
+  row2_value: row2,
+  row3_label: 'All Time Daddy 🏆',
+  row3_value: row3
+});
