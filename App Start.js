419e46598d7c7675ca2c7e3e2c89bd0fe107fc39
KinkZilla <realcjbaker@gmail.com>
2025-05-13 21:47:17 -0700

    Update App Start.js

--- App Start.js
+++ App Start.js
@@ -1,8 +1,31 @@
-if ($kv.get('appSessionInitialized') === false) {
+   
+    if ($settings.tip_menu === true) {
+    let menu = [];
+
+    try {
+      menu = JSON.parse($kv.get('tipMenu') || '[]');
+    } catch (e) {
+      menu = [];
+    }
+
+    if (!Array.isArray(menu) || menu.length === 0) {
+      sendMsg('tipMenuWelcome');
+    } else {
+      $callback.create('tipMenuAnnc', 60 * $settings.tip_menu_time, true);
+      
+    }
+    }// end
+
+
   initializeAppSessionState(); 
-}
+
      // 🧱 Prep all KV
 sendMsg('appStarted');
+ 
+
+
+ 
+
 // ✅ After all that, THEN create the introDaddyPrompt callback
 if ($kv.get('crownUnlocked') !== 'true') {
   $callback.cancel('introDaddyPrompt');
