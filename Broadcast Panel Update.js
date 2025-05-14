b35929a23f59bc8f24817e41cd6ecbb73698db43
KinkZilla <realcjbaker@gmail.com>
2025-05-13 21:48:00 -0700

    Update Broadcast Panel Update.js

--- Broadcast Panel Update.js
+++ Broadcast Panel Update.js
@@ -1,13 +1,37 @@
-const row1 = $kv.get('panel_row1_value') || '';
-const row2 = $kv.get('panel_row2_value') || '';
-const row3 = $kv.get('panel_row3_value') || '';
+let row1_label = '';
+let row1 = '';
+let row2_label = '';
+let row2 = '';
+let row3_label = '';
+let row3 = '';
+
+try { row1_label = $kv.get('row1_label_value') || ''; } catch (e) {
+console.log('Failed to get row1_label_value');
+}
+try { row1 = $kv.get('panel_row1_value') || ''; } catch (e) {
+console.log('Failed to get panel_row1_value');
+}
+
+try { row2_label = $kv.get('row2_label_value') || ''; } catch (e) {
+console.log('Failed to get row2_label_value');
+}
+try { row2 = $kv.get('panel_row2_value') || ''; } catch (e) {
+console.log('Failed to get panel_row2_value');
+}
+
+try { row3_label = $kv.get('row3_label_value') || ''; } catch (e) {
+console.log('Failed to get row3_label_value');
+}
+try { row3 = $kv.get('panel_row3_value') || ''; } catch (e) {
+console.log('Failed to get panel_row3_value');
+}
 
 $room.setPanelTemplate({
   template: '3_rows_of_labels',
-  row1_label: 'Received / Goal',
+  row1_label: row1_label,
   row1_value: row1,
-  row2_label: 'Current Daddy',
+  row2_label: row2_label,
   row2_value: row2,
-  row3_label: 'All Time Daddy 🏆',
+  row3_label: row3_label,
   row3_value: row3
 });
